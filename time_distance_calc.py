4850e2d048babcf798bc75b87c4a761220249c0b
Jamie Hirst <jamie.hirst10@gmail.com>
2022-01-07 20:39:30 +0000

    added doc strings and removed unneeded return/variable assignment

--- time_distance_calc.py
+++ time_distance_calc.py
@@ -21,19 +21,27 @@ class TimeDistanceCalculator:
         self.userID_list = []
 
     def dist_time_finder(self, origin, destination):
+        '''
+        Takes two postcodes and returns the time and distance by road as determined by google maps distance matrix
+        '''
         dist_time_matrix = self.gmaps.distance_matrix(origin, destination, mode='driving', units='imperial', region='gb')
         distance = dist_time_matrix['rows'][0]['elements'][0]['distance']['text']
         time = dist_time_matrix['rows'][0]['elements'][0]['duration']['text']
         return time, distance
 
     def get_data(self, userID_cell, origin_cell, destination_cell):
+        '''
+        Retrieve data from excel, into lists which are combined and returned as nested lists, each nested list being a row
+        '''
         userID_list = [cell.value for cell in self.ws[userID_cell]]
         origin_list = [cell.value for cell in self.ws[origin_cell]]
         destination_list = [cell.value for cell in self.ws[destination_cell]]
-        combined_list = list(map(list, zip(userID_list, origin_list, destination_list)))
-        return combined_list
+        self.combined_list = list(map(list, zip(userID_list, origin_list, destination_list)))
     
     def origin_loc_check(self):
+        '''
+        Check origin postcode against know area codes for invalid addresses, see if a refined area code is valid and if so append nested list of row data to new checked list
+        '''
         nomi = pgeocode.Nominatim('gb')
         for code in self.combined_list:
             if code[1] == None:
@@ -52,6 +60,9 @@ class TimeDistanceCalculator:
                     self.orig_chk_combined_list.append(code)
 
     def destination_loc_check(self):
+        '''
+        Check destination postcode against known area codes for invalid addresses, see if a refined area code is valid and if so append nested list of row data to new checked list
+        '''
         nomi = pgeocode.Nominatim('gb')
         for code in self.orig_chk_combined_list:
             if code[2] == None:
@@ -74,6 +85,9 @@ class TimeDistanceCalculator:
                     self.dest_chk_combined_list.append(code)
 
     def calc_time_dist(self):
+        '''
+        Put origin and destination postcode into dist_time_finder, then add time, distance and user id to separate lists ready to be written to excel
+        '''
         for row in self.dest_chk_combined_list:
             try:
                 time, distance = self.dist_time_finder(row[1], row[2])
@@ -84,6 +98,9 @@ class TimeDistanceCalculator:
                 pass 
 
     def write_to_excel(self, write_file_name):
+        '''
+        Write user id, travel time and travel distance to rows in a new excel
+        '''
         workbook = xlsxwriter.Workbook(write_file_name)
         worksheet = workbook.add_worksheet()
         worksheet.write_column('A1', self.userID_list)
@@ -92,9 +109,10 @@ class TimeDistanceCalculator:
         workbook.close()
 
     def postcode_running(self, userID_cell, origin_cell, destination_cell, write_file_name):
-        self.combined_list = self.get_data(userID_cell, origin_cell, destination_cell)
+        self.get_data(userID_cell, origin_cell, destination_cell)
         self.origin_loc_check()
         self.destination_loc_check()
         print(self.dest_chk_combined_list)
         self.calc_time_dist()
         self.write_to_excel(write_file_name)
+        
